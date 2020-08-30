5ec629d081b7bcc86e2e1affa2f168e1e43e45be
geondongK <ehd563@naver.com>
2020-08-30 17:04:44 +0900

    incomplete

--- syntax/index.js
+++ syntax/index.js
@@ -6,24 +6,31 @@ const app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var title = queryData.id;
+  console.log(queryData);
   console.log(queryData.id);
   if (_url == '/') {
-    title = 'Welcome'
+    title = 'Welcome';
   }
   if (_url == '/favicon.ico') {
     return response.writeHead(404);
   }
   response.writeHead(200);
-  fs.readFile(`data/${title}`, 'utf8', function(err, description) {
-    var template = `
+  fs.readFile(`../data/${queryData.id}`, 'utf8', function (err,description) {
+  // fs.readFile('data/${title}', 'utf8', function(err, description) {
+  console.log(description);
+    if (err) {
+      return console.log(err);
+    }
+
+     var template = `
     <!doctype html>
     <html>
-    
+
     <head>
       <title>WEB1 - ${title}</title>
       <meta charset="utf-8">
     </head>
-    
+
     <body>
       <h1><a href="/">WEB</a></h1>
       <ul>
@@ -34,11 +41,11 @@ const app = http.createServer(function (request, response) {
       <h2>${title}</h2>
       <p>${description}</p>
     </body>
-    
-    </html>`;
-    response.end(template);
-  })
 
+    </html>
+    `;
+    response.end(template);
+   })
 
 });
 app.listen(3000);
