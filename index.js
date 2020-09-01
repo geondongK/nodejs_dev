59396083008284a20faf95dd49ff62abf6f950ed
geondongK <ehd563@naver.com>
2020-09-02 03:18:21 +0900

    incompletion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 // const { query } = require('express');
 function templateHTML(title, list, body) {
   return `
@@ -15,6 +16,7 @@ function templateHTML(title, list, body) {
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             ${body}
           </body>
       
@@ -44,7 +46,7 @@ var app = http.createServer(function (request, response) {
   if (pathname == '/') {
     if (queryData.id == undefined) {
 
-      fs.readdir('../data', function (error, filelist) {
+      fs.readdir('data', function (error, filelist) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
@@ -54,8 +56,8 @@ var app = http.createServer(function (request, response) {
         response.end(template);
       })
     } else {
-      fs.readdir('../data', function (error, filelist) {
-        fs.readFile(`../data/${queryData.id}`, 'utf8', function (err, description) {
+      fs.readdir('data', function (error, filelist) {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var list = templateList(filelist);
           var title = queryData.id;
           var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
@@ -64,7 +66,44 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
-  } else {
+  } else if (pathname == '/create') {
+    fs.readdir('data', function (error, filelist) {
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+      response.writeHead(200);
+      response.end(template);
+    })
+
+  } else if (pathname == '/create_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      })
+    });
+
+
+
+  }
+  else {
     response.writeHead(404);
     response.end('Not found');
   }
