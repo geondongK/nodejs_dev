996ff3969b3d11efa8a12b21ce91b463a06d84fc
geondongK <ehd563@naver.com>
2020-09-05 02:32:08 +0900

    incompletion

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ var qs = require('querystring');
 // const { query } = require('express');
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
-
   if (pathname == '/') {
     if (queryData.id == undefined) {
-
-      fs.readdir('data', function (error, filelist) {
+      fs.readdir('./data', function (error, filelist) {
         var title = 'Welcome';
         var description = 'Hello. Node.js';
-        var list = template.List(filelist);
+        var list = template.list(filelist);
         var html = template.HTML(title, list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`
@@ -27,17 +25,22 @@ var app = http.createServer(function (request, response) {
         response.end(html);
       });
     } else {
-      fs.readdir('data', function (error, filelist) {
+      fs.readdir('./data', function (error, filelist) {
+        var filteredId = path.parse(queryData.id).base;
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-          var list = template.List(filelist);
           var title = queryData.id;
+          var sanitizedTitle = sanitizeHtml(title);
+          var sanitizedDescription = sanitizeHtml(description, {
+            allowedTags:['h1']
+          });
+          var list = template.list(filelist);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `<a href="/create">create</a> 
-            <a href="/update?id=${title}">update</a>
-            <form action="delete_process" method="post" onsubmit="삭제하시겠습니까?">
-              <input type="hidden" name="id" value="${title}">
+            <a href="/update?id=${sanitizedTitle}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
           </form>`
           );
@@ -47,11 +50,10 @@ var app = http.createServer(function (request, response) {
       });
     }
   } else if (pathname == '/create') {
-    fs.readdir('data', function (error, filelist) {
+    fs.readdir('./data', function (error, filelist) {
       var title = 'WEB - create';
-      var list = template.List(filelist);
-      var html = template.HTML(title, list,      
-      `
+      var list = template.list(filelist);
+      var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -65,7 +67,6 @@ var app = http.createServer(function (request, response) {
       response.writeHead(200);
       response.end(html);
     })
-
   } else if (pathname == '/create_process') {
     var body = '';
     request.on('data', function (data) {
@@ -74,20 +75,18 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var title = post.title;
-      console.log(title)
       var description = post.description;
       fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
       })
     });
-  }
-  else if (pathname == '/update') {
-    fs.readdir('data', function (error, filelist) {
+  } else if (pathname == '/update') {
+    fs.readdir('./data', function (error, filelist) {
       var filteredId = path.parse(queryData.id).base;
       fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var list = template.List(filelist);
         var title = queryData.id;
+        var list = template.list(filelist); 
         var html = template.HTML(title, list,
           `
           <form action="/update_process" method="post">
@@ -124,7 +123,7 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end();
         })
-      })
+      });
     });
   } else if (pathname == '/delete_process') {
     var body = '';
